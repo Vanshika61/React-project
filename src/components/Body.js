4807d735a5d989bef12d1a03db7262e8d05bb6c6
Vanshika61 <agrawal10614@gmail.com>
2024-07-26 18:19:36 +0530

    add the logic of restaurant not found when the filtered restaurant is not present

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,74 +1,85 @@
-// Default import 
+// Default import
 import RestaurantCard from "./RestaurantCard";
-// Named import 
-import {restaurantList} from "../constants";
-import  {useEffect, useState}  from "react";
+// Named import
+import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
-
-function filterData(searchTxt, restaurants){
-  const filterData = restaurants.filter((restaurant)=>
+function filterData(searchTxt, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
     restaurant?.info?.name?.toLowerCase()?.includes(searchTxt.toLowerCase())
   );
 
   return filterData;
 }
 
-const Body =()=>{
-
+const Body = () => {
   const [searchTxt, setSearchTxt] = useState("");
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     // console.log("useEffect");
     // Api call       best place to call the api inside the useEffect function or hook
     getRestaurants();
   }, []);
 
   // Function of api calling
-  async function getRestaurants(){
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1986817&lng=78.160006&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1986817&lng=78.160006&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     console.log(json);
     // optional chaining
-    setAllRestaurants(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setFilteredRestaurants(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setAllRestaurants(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   }
 
-  console.log("render()");   // react will re-render everything very quickly // it reloads basically
+  console.log("render()"); // react will re-render everything very quickly // it reloads basically
 
-  return (allRestaurants.length === 0)? (<Shimmer/>) :(
-      <>
+  //Early return
+  if(!allRestaurants) return null;
+
+  return allRestaurants.length === 0 ? (
+    <Shimmer/>
+  ) : (
+    <>
       <div className="search-container">
-        <input 
-          type="text" 
-          className="search-input" 
-          placeholder="search" 
-          value={searchTxt} 
-          onChange={(e)=>{
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchTxt}
+          onChange={(e) => {
             setSearchTxt(e.target.value);
           }}
         />
-        <button className="search-btn" onClick={()=>{
-          // need to filter the data
-          const data = filterData(searchTxt, allRestaurants);
-          // update the state - restaurants
-          setFilteredRestaurants(data);
-
-        }}>Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // need to filter the data
+            const data = filterData(searchTxt, allRestaurants);
+            // update the state - restaurants
+            setFilteredRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
 
-        <div className="restaurant-list">
-          {
-            filteredRestaurants.map((restaurant)=>{
-              return <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
-            })
-          }
-        </div>
-      </>
-    );
+      <div className="restaurant-list">
+        {filteredRestaurants.length===0 ? (<h1>Restaurant not found</h1>) : (filteredRestaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
+          );
+        }))}
+      </div>
+    </>
+  );
 };
 
 export default Body;
