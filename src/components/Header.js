b0bd06021a2e08f60050b7d169a70e241b5962db
Vanshika61 <agrawal10614@gmail.com>
2024-08-27 12:43:57 +0530

    making show and hide functionality for every section by using state

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logoImage from "./assets/Logo.jpg";
 const Logo=()=>{
     return(
         <Link to="/">
-            <img className="logo" src={logoImage} alt="logo" />
+            <img className="h-28 p-2" src={logoImage} alt="logo" />
         </Link>
     );
 };
@@ -12,15 +12,15 @@ const Logo=()=>{
 
 const Header =()=>{
     return(
-        <div className="header">
+        <div className="flex justify-between bg-gray-100 shadow-lg">
             <Logo/>         {/* Component composition or composing component*/}
-            <div className="nav-items">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/instamart">Instamart</Link></li>
-                    <li>Cart</li>
+            <div>
+                <ul className="flex py-10">
+                    <li className="px-2"><Link to="/">Home</Link></li>
+                    <li className="px-2"><Link to="/about">About</Link></li>
+                    <li className="px-2"><Link to="/contact">Contact</Link></li>
+                    <li className="px-2"><Link to="/instamart">Instamart</Link></li>
+                    <li className="px-2">Cart</li>
                 </ul>
             </div>
         </div>
--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
+const Section =({title, description})=>{
+
+  const [isVisible, setIsVisible]=useState(false);
+
+  return(
+    <div className="border border-black p-2 m-2">
+      <h1 className="font-bold">{title}</h1>
+      {isVisible && <p>{description}</p>}
+      {isVisible ? <button className="underline" onClick={ ()=> setIsVisible(false)}>Hide</button> : <button className="underline" onClick={()=> setIsVisible(true)}>Show</button>}
+           
+    </div>
+  );
+}
+
+
+
 const Instamart = () => {
+
   return (
     <div>
-      <h1>Instamart</h1>
-      <h1>100s of components</h1>
+      <h1 className="text-3xl font-bold text-center p-2 m-2">Instamart</h1>
+      <Section title={"About Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
+
+
+      <Section title={"Team Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
+
+
+      <Section title={"Career Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
     </div>
   );
 };
