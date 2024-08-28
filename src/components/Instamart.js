440604d8bd9a7304ed0249edc2048621f29cb25b
Vanshika61 <agrawal10614@gmail.com>
2024-08-28 17:37:10 +0530

    this is lifting the state up but we need to do this code readable

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
-const Section =({title, description})=>{
+const Section =({title, description, isVisible, setIsVisible})=>{
 
-  const [isVisible, setIsVisible]=useState(false);
 
   return(
     <div className="border border-black p-2 m-2">
@@ -18,16 +17,50 @@ const Section =({title, description})=>{
 
 const Instamart = () => {
 
+  const [sectionConfig, setSectionConfig]=useState({
+    showAbout : false,
+    showTeam : false,
+    showCareer : false,
+  });
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-center p-2 m-2">Instamart</h1>
-      <Section title={"About Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
+      <Section title={"About Instamart"}
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
+      isVisible={sectionConfig.showAbout} 
+      setIsVisible={ ()=>{
+        setSectionConfig({
+          showAbout: true,
+          showTeam: false,
+          showCareer: false
+        })}
+      }/>
 
 
-      <Section title={"Team Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
+      <Section title={"Team Instamart"} 
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
+      isVisible={sectionConfig.showTeam}
+      setIsVisible={()=>{
+        setSectionConfig({
+          showAbout:false,
+          showTeam:true,
+          showCareer:false
+        })
+      }}/>
 
 
-      <Section title={"Career Section"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} />
+
+      <Section title={"Careers"} 
+      description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
+      isVisible={sectionConfig.showCareer}
+      setIsVisible={()=>{
+        setSectionConfig({
+          showAbout:false,
+          showTeam:false,
+          showCareer:true
+        })
+      }}/>
     </div>
   );
 };
