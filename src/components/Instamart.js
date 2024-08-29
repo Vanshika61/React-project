ab5f3a247b143db5880fceff3eeaf4859aefaf63
Vanshika61 <agrawal10614@gmail.com>
2024-08-29 14:18:10 +0530

    lifting state up more efficient

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -17,50 +17,28 @@ const Section =({title, description, isVisible, setIsVisible})=>{
 
 const Instamart = () => {
 
-  const [sectionConfig, setSectionConfig]=useState({
-    showAbout : false,
-    showTeam : false,
-    showCareer : false,
-  });
+  const [visibleSection, setVisibleSection]=useState("about");
 
   return (
     <div>
       <h1 className="text-3xl font-bold text-center p-2 m-2">Instamart</h1>
       <Section title={"About Instamart"}
       description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
-      isVisible={sectionConfig.showAbout} 
-      setIsVisible={ ()=>{
-        setSectionConfig({
-          showAbout: true,
-          showTeam: false,
-          showCareer: false
-        })}
-      }/>
+      isVisible={visibleSection === "about"} 
+      setIsVisible={ ()=>{ setVisibleSection("about") }}/>
 
 
       <Section title={"Team Instamart"} 
       description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
-      isVisible={sectionConfig.showTeam}
-      setIsVisible={()=>{
-        setSectionConfig({
-          showAbout:false,
-          showTeam:true,
-          showCareer:false
-        })
-      }}/>
+      isVisible={visibleSection==="team"}
+      setIsVisible={()=>{ setVisibleSection("team") }}/>
 
 
 
       <Section title={"Careers"} 
       description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro, cum optio vero id debitis expedita magni amet similique dolorum odit, eaque molestiae provident reprehenderit perspiciatis aperiam cumque fuga repellendus?"} 
-      isVisible={sectionConfig.showCareer}
-      setIsVisible={()=>{
-        setSectionConfig({
-          showAbout:false,
-          showTeam:false,
-          showCareer:true
-        })
-      }}/>
+      isVisible={visibleSection==="career"}
+      setIsVisible={()=>{ setVisibleSection("career") }}/>
     </div>
   );
 };
